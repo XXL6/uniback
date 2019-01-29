e5fea18e3bd708b1b6627fc0686cf1a1b8d640c8
Arnas Babeckis <xxl606@gmail.com>
2019-01-29 00:18:00 -0500

    Started work on logging functionality

--- uniback/__init__.py
+++ uniback/__init__.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from uniback.config import Config
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import logging
+from uniback import ub_logging
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+ub_logging.initialize_logging()
+logger = logging.getLogger('mainLogger')
 
 
 def create_app(config_class=Config):
@@ -34,5 +37,5 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     with app.app_context():
         db.create_all()
-
+    logger.info('App created ayy lmao')
     return app
--- uniback/blueprints/backup/routes.py
+++ uniback/blueprints/backup/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, Blueprint
+import logging
 
 backup = Blueprint('backup', '__name__')
+logger = logging.getLogger('mainLogger')
 
 
 @backup.route(f'/{backup.name}/saved_jobs')
@@ -10,4 +12,5 @@ def saved_jobs():
 
 @backup.route(f'/{backup.name}/job_history')
 def job_history():
+    logger.info('entered the job history page')
     return render_template('backup/job_history.html')
