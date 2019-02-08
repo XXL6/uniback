fcfccb3bce0aedd5c18bd2a837efa15b74740811
Arnas Babeckis <xxl606@gmail.com>
2019-02-08 00:34:03 -0500

    Started prototyping a backup engine plugin

--- uniback/plugins/engines/robocopy.py
+++ uniback/plugins/engines/robocopy.py
@@ -1,9 +1,25 @@
+import subprocess
 
 
 class Robocopy:
 
     OS_SUPPORT = ["Windows"]
 
-    def __init__(self, repository, backup_set):
+    def __init__(
+            self,
+            repository,
+            backup_set,
+            logging=None,
+            progress_tracker=None):
         self.repository = repository
         self.backup_set = backup_set
+        if logging is not None:
+            self.logging_enabled = True
+
+    def run(self):
+        self.task = subprocess.Popen(
+            ['robocopy', 'C:\\Users\\Arnas\\Downloads\\test',
+                'C:\\Users\\Arnas\\Downloads\\test2'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+        
